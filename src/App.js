63a382460af0da9291d34a311ef089e051fe26f4
Jerboa <lotaijo@gmail.com>
2022-09-05 11:03:22 +1200

    Adding Navigation Component, Router Link, Logo. Styling.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return <p>I am the shop page</p>;
+};
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
