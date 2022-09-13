cca868aed310a5c71c9d97796540068b1838af99
Jerboa <lotaijo@gmail.com>
2022-09-13 17:29:11 +1200

    Sign up form pt.1

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -8,26 +8,57 @@ const defaultFormFields = {
 };
 
 const SignUpFrom = () => {
-  const [fromField, setFromField] = useState(defaultFormFields);
-  const { displayName, email, password, confirmPassword } = fromField;
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
 
-  const handleChnage = (event) => {};
+  console.log(formFields);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    // console.log(event);
+
+    setFormFields({ ...formFields, [name]: value });
+  };
 
   return (
     <div>
       <h1>Sign up with your email and password</h1>
       <form onSubmit={() => {}}>
         <label>Display Name</label>
-        <input type="text" required onChange={handleChnage} />
+        <input
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
 
         <label>Email</label>
-        <input type="email" required onChange={handleChnage} />
+        <input
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
 
         <label>Password</label>
-        <input type="password" required onChange={handleChnage} />
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
 
         <label>Confirmed Password</label>
-        <input type="password" required onChange={handleChnage} />
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
 
         <button type="submit">Sign Up</button>
       </form>
