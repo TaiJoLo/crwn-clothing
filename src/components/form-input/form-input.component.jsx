d50cd3252373e14944bdadbc8e2a96a166623645
Jerboa <lotaijo@gmail.com>
2022-09-28 17:43:33 +1300

    Generalizing Form Input Component

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,20 @@
+import "./form-input.styles.scss";
+
+const FormInput = ({ label, otherInputs }) => {
+  return (
+    <div className="group">
+      <input className="form-input" {...otherInputs} />
+      {label && (
+        <label
+          className={`${
+            otherInputs.value.length ? "shrink" : ""
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      )}
+    </div>
+  );
+};
+
+export default FormInput;
