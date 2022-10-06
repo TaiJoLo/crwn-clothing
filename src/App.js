a20a76daa7391ef3a9afc167956f8e7225944263
Jerboa <lotaijo@gmail.com>
2022-10-06 17:43:15 +1300

    Created new shop page, products context.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 
-const Shop = () => {
-  return <p>I am the shop page</p>;
-};
+import Shop from "./routes/shop/shop.component";
 
 function App() {
   return (
