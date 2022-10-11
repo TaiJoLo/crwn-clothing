84e116aa2760cac119d688221124cc2a11f0a671
Jerboa <lotaijo@gmail.com>
2022-10-11 17:57:16 +1300

    Added addToCart feature and cart item designs

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,16 +4,16 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import { CartContext } from "../../contexts/cart.context";
 
-import "./cart-icon.sytles.scss";
+import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <sapn classNmae="item-count">0</sapn>
+      <sapn classNmae="item-count">{cartCount}</sapn>
     </div>
   );
 };
