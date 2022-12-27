4d0e26c04415d50b2a7d5f170c6ef55cf65bd9d6
LOLO <lotaijo@gmail.com>
2022-12-27 15:33:11 +1300

    switching categories from redux-thunk to redux-saga

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -7,13 +7,13 @@ import CategoriesPreview from "../categories-preview/categories-preview.componen
 
 import "./shop.styles.scss";
 
-import { fetchCategoriesAsync } from "../../store/categories/category.action";
+import { fetchCategoriesStart } from "../../store/categories/category.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchCategoriesAsync());
+    dispatch(fetchCategoriesStart());
   }, [dispatch]);
 
   return (
