763adeeab703fe0526bf8587b854d59a46334b82
LOLO <lotaijo@gmail.com>
2023-01-09 21:14:24 +1300

    typing SVG imports

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -1,19 +1,21 @@
-import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 
 import {
-  selectIsCartOpen,
   selectCartCount,
+  selectIsCartOpen,
 } from "../../store/cart/cart.selector";
-
 import { setIsCartOpen } from "../../store/cart/cart.action";
 
-import { ShoppingIcon, CartIconContainer, ItemCount } from "./cart-icon.styles";
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+
+import { CartIconContainer, ItemCount } from "./cart-icon.styles";
 
 const CartIcon = () => {
   const dispatch = useDispatch();
-  const isCartOpen = useSelector(selectIsCartOpen);
+
   const cartCount = useSelector(selectCartCount);
+  const isCartOpen = useSelector(selectIsCartOpen);
 
   const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
 
