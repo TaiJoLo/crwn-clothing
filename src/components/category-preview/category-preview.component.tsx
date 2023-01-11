ca7092e17e6ddba042e602ea411c1163dd37ce6d
LOLO <lotaijo@gmail.com>
2023-01-12 07:04:27 +1300

    converting reamining component to typescript 2

--- src/components/category-preview/category-preview.component.tsx
+++ src/components/category-preview/category-preview.component.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FC } from "react";
 import ProductCard from "../product-card/product-card.component";
 import {
   CategoryPreviewContainer,
@@ -6,7 +7,14 @@ import {
   Preview,
 } from "./category-preview.styles";
 
-const CategoryPreview = ({ title, products }) => {
+import { CategoryItem } from "../../store/categories/category.types";
+
+type CategoryPreviewProps = {
+  title: string;
+  products: CategoryItem[];
+};
+
+const CategoryPreview: FC<CategoryPreviewProps> = ({ title, products }) => {
   return (
     <CategoryPreviewContainer>
       <h2>
