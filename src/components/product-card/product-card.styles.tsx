22bf848601587b71cb55d4142cde196ca9b9fa9e
Jerboa <lotaijo@gmail.com>
2023-01-12 15:12:20 +1300

    updating styles to use media queries for mobile

--- src/components/product-card/product-card.styles.tsx
+++ src/components/product-card/product-card.styles.tsx
@@ -19,6 +19,14 @@ export const ProductCartContainer = styled.div`
     position: absolute;
     top: 255px;
     display: none;
+
+    @media screen and (max-width: 800px) {
+      display: block;
+      opactiy: 0.9;
+      min-width: unset;
+      padding: 0 10px;
+      font-size: 12px;
+    }
   }
   &:hover {
     img {
@@ -27,6 +35,19 @@ export const ProductCartContainer = styled.div`
     button {
       opacity: 0.85;
       display: flex;
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    width: 40vw;
+
+    &:hover {
+      img {
+        opacity: unset;
+      }
+      button {
+        opacity: unset;
+      }
     }
   }
 `;
